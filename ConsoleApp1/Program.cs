47eecc869b14c669cb85a89261f3dc04b09402b5
millerfridrich <millerfridrich@gmail.com>
2026-06-25 12:05:20 +0500

    Первый коммит

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string name = "Nishon";
+        int num1 = 7;
+        int num2 = 5;
+
+        Additional(num1, num2);
+    }
+
+    static int Additional(int a, int b)
+    {
+        Console.WriteLine("Сумма равна " + (a + b));
+        return a + b;
+    }
+}
