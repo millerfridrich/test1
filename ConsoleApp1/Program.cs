3b910bdaa5963c07fcde26a2ec9d0bf33d7c87da
millerfridrich <millerfridrich@gmail.com>
2026-06-25 12:26:51 +0500

    Второй коммит.
    Добавил функцию умножения двух чисел.
    
    Умножение чисел между собой.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,11 +9,18 @@ class Program
         int num2 = 5;
 
         Additional(num1, num2);
+        Multiply(num1, num2);
     }
 
     static int Additional(int a, int b)
     {
-        Console.WriteLine("Сумма равна " + (a + b));
+        Console.WriteLine("Сумма равна " + (a + b) + "\n");
         return a + b;
+    }
+
+    static int Multiply(int a, int b)
+    {
+        Console.WriteLine("Произведение равно " + (a * b) + "\n");
+        return a * b;
     }
 }
