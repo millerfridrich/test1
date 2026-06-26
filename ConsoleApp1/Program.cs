a09d47e70499678026ced9a72b32d4ac95edfcd6
millerfridrich <millerfridrich@gmail.com>
2026-06-26 10:01:21 +0500

    Четвёртый коммит.
    Добавлена функция вычитания.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,13 +4,13 @@ class Program
 {
     static void Main()
     {
-        string name = "Nishon";
         int num1 = 7;
         int num2 = 5;
 
         Additional(num1, num2);
         Multiply(num1, num2);
         Divide(num1, num2);
+        Subtraction(num1, num2);
     }
 
     static int Additional(int a, int b)
@@ -19,6 +19,11 @@ class Program
         return a + b;
     }
 
+    static int Subtraction(int a, int b)
+    {
+        Console.WriteLine("Вычитание равно " + (a - b) + "\n");
+        return a - b;
+    }
     static int Multiply(int a, int b)
     {
         Console.WriteLine("Произведение равно " + (a * b) + "\n");
